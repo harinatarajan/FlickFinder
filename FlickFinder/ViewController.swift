0d99b56dd61f186df8ea6df02de0d17657886f57
harinatarajan <hari.natarajan@appunu.com>
2015-06-05 12:59:35 -0700

    Initial check in

--- FlickFinder/ViewController.swift
+++ FlickFinder/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flick: UIImageView!
+    @IBOutlet weak var flickName: UILabel!
+    @IBOutlet weak var searchFlickText: UITextField!
+    
+    @IBOutlet weak var searchFlickLatitude: UITextField!
+    @IBOutlet weak var searchFlickLongitude: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func searchFlickByText() {
+        println("Flick for \(self.searchFlickText.text)")
     }
 
+    @IBAction func searchFlickByLatLng() {
+        println("Flick at \(self.searchFlickLatitude.text) . \(self.searchFlickLongitude.text)")
+    }
 
 }
 
